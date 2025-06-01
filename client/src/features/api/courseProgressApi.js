96a14beef73a118377dea7d1d0c2260468f67264
nvggupta <ng969028@gmail.com>
2025-06-01 16:49:10 +0530

    change URL

--- client/src/features/api/courseProgressApi.js
+++ client/src/features/api/courseProgressApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PROGRESS_API = "http://localhost:3000/api/v1/progress";
+const COURSE_PROGRESS_API = `${import.meta.env.VITE_API_URL}/api/v1/progress`;
 
 export const courseProgressApi = createApi({
   reducerPath: "courseProgressApi",
--- client/src/features/api/purchaseApi.js
+++ client/src/features/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "http://localhost:3000/api/v1/purchase";
+const COURSE_PURCHASE_API = `${import.meta.env.VITE_API_URL}/api/v1/purchase`;
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
